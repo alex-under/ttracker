79fd020d7cabf67b5da9eafe84ce7e974a5906de
alex-nikolaev <alex-nikolaev@inbox.ru>
2018-05-06 23:26:21 +0400

    refactor key hook

--- src/main/kotlin/ru/alexunder/ttracker/ui/GlobalKeyHook.kt
+++ src/main/kotlin/ru/alexunder/ttracker/ui/GlobalKeyHook.kt
@@ -11,47 +11,69 @@ import java.util.logging.Level
 import java.util.logging.Logger
 
 
-fun registerKeyHook(stage: Stage) {
-    try {
-        muteJnativeHookLogger()
-        GlobalScreen.registerNativeHook()
-        GlobalScreen.addNativeKeyListener(GlobalKeyListener(stage))
-    } catch (ex: NativeHookException) {
-        println("There was a problem registering the native hook: ${ex.message}")
-        System.exit(1)
+class GlobalKeyHook(stage: Stage) {
+    private val keyListener = GlobalKeyListener(stage)
+
+    init {
+        muteLogger()
+    }
+
+    fun registerKeyHook() {
+        try {
+            GlobalScreen.registerNativeHook()
+            GlobalScreen.addNativeKeyListener(keyListener)
+        } catch (ex: NativeHookException) {
+            println("There was a problem registering the native hook: ${ex.message}")
+            System.exit(1)
+        }
+    }
+
+    fun unregisterKeyHook() {
+        GlobalScreen.removeNativeKeyListener(keyListener)
+        GlobalScreen.unregisterNativeHook()
+    }
+
+    private fun muteLogger() {
+        val logger = Logger.getLogger(GlobalScreen::class.java.`package`.name)
+        logger.level = Level.WARNING
+        logger.useParentHandlers = false
     }
 }
 
-fun muteJnativeHookLogger() {
-    val logger = Logger.getLogger(GlobalScreen::class.java.`package`.name)
-    logger.level = Level.WARNING
-    logger.useParentHandlers = false
+class GlobalKeyListener(private val stage: Stage) : NativeKeyListener {
+    private val eventConsumer = EventConsumer()
+
+    override fun nativeKeyPressed(event: NativeKeyEvent) {
+        if (event.keyCode != NativeKeyEvent.VC_W ||
+                event.modifiers and NativeKeyEvent.META_MASK == 0) {
+            return
+        }
+
+        JavaFX.dispatch {
+            if (stage.isShowing) {
+                stage.hide()
+            } else {
+                stage.isAlwaysOnTop = true
+                stage.show()
+                stage.toFront()
+                stage.requestFocus()
+            }
+        }
+        eventConsumer.tryConsumeEvent(event)
+    }
+
+    override fun nativeKeyReleased(e: NativeKeyEvent) {}
+    override fun nativeKeyTyped(e: NativeKeyEvent) {}
 }
 
-
-class GlobalKeyListener(private val stage: Stage) : NativeKeyListener {
-
+class EventConsumer {
     private val field = NativeInputEvent::class.java.getDeclaredField("reserved")
 
     init {
         field.isAccessible = true
     }
 
-    override fun nativeKeyPressed(e: NativeKeyEvent) {
-        if (e.keyCode != NativeKeyEvent.VC_W ||
-                e.modifiers and NativeKeyEvent.META_MASK == 0) {
-            return
-        }
-
-        JavaFX.dispatch {
-            stage.show()
-        }
+    fun tryConsumeEvent(e: NativeKeyEvent) {
         field.setShort(e, 0x01.toShort()) // try to consume event
-    }
-
-    override fun nativeKeyReleased(e: NativeKeyEvent) {
-    }
-
-    override fun nativeKeyTyped(e: NativeKeyEvent) {
     }
 }
--- src/main/kotlin/ru/alexunder/ttracker/ui/TaskTrackerApp.kt
+++ src/main/kotlin/ru/alexunder/ttracker/ui/TaskTrackerApp.kt
@@ -7,12 +7,14 @@ import tornadofx.*
 class TaskTrackerApp : App(TaskSelectorView::class) {
 
     private lateinit var tray: SysTray
+    private lateinit var keyHook: GlobalKeyHook
 
     override fun start(stage: Stage) {
         super.start(stage)
         Platform.setImplicitExit(false)
         stage.hide()
         tray = SysTray(stage)
-        registerKeyHook(stage)
+        keyHook = GlobalKeyHook(stage)
+        keyHook.registerKeyHook()
     }
 }
