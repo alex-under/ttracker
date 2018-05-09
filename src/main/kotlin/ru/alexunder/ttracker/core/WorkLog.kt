a051a19018fbaf2c7edb2815e404ff0988a99517
alex-nikolaev <alex-nikolaev@inbox.ru>
2018-05-09 22:42:26 +0400

    predefined work log for tests

--- src/main/kotlin/ru/alexunder/ttracker/core/WorkLog.kt
+++ src/main/kotlin/ru/alexunder/ttracker/core/WorkLog.kt
@@ -6,6 +6,12 @@ object WorkLog {
 
     private val workItems : MutableList<WorkItem> = ArrayList()
 
+    init {
+        workItems.add(WorkItem(Task(100, "work item 1"), LocalDateTime.now().minusMinutes(1), LocalDateTime.now()))
+        workItems.add(WorkItem(Task(101, "work item 2"), LocalDateTime.now().minusMinutes(1), LocalDateTime.now()))
+        workItems.add(WorkItem(Task(102, "work item 3"), LocalDateTime.now().minusMinutes(1), LocalDateTime.now()))
+    }
+
     fun addItem(task: Task, from: LocalDateTime, to: LocalDateTime) {
         workItems.add(WorkItem(task, from, to))
     }
